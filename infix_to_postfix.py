ba560558d11f15a2401e690af6c9e69c5ef44543
Swarnim Dubey <151605192+Swarnim-Dubey@users.noreply.github.com>
2025-10-18 06:12:43 +0530

    Add files via upload

---
+++ infix_to_postfix.py
@@ -0,0 +1,38 @@
+def precedence(operator):
+    if operator == '+' or operator == '-':
+        return 1
+    if operator == '*' or operator == '/':
+        return 2
+    if operator == '^':
+        return 3
+    return 0
+
+def infix_to_postfix(expression):
+    # stack for operators
+    stack = []
+
+    # list for output
+    output = []
+
+    for char in expression:
+        if char.isalnum():
+            output.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        else:
+            while stack and precedence(char) <= precedence(stack[-1]):
+                output.append(stack.pop())
+            stack.append(char)
+    while stack:
+        output.append(stack.pop())
+    return ''.join(output)
+
+
+expression = input("Enter infix expression: ")
+postfix = infix_to_postfix(expression)
+print("Postfix expression:", postfix)
+

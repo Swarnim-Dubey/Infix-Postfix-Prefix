79b74780a58d3974069df425276bda066bf79872
unknown <swarnimdubey135@gmail.com>
2025-11-25 21:23:26 +0530

    completed

--- postfix_evaluation.py
+++ postfix_evaluation.py
@@ -1,3 +1,12 @@
 # postfix expression evaluation using stacks 
 
+def post_evaluation(expression):
+    stack = []
 
+
+
+
+# taking the input of the expression
+
+expression = input("Enter postfix expression : ")
+print("Evaluated answer to the expression : ", post_evaluation(expression))

a8e42bd2051a2c7a41ca813e837d75867845263c
unknown <swarnimdubey135@gmail.com>
2025-11-25 21:24:13 +0530

    completed

--- postfix_evaluation.py
+++ postfix_evaluation.py
@@ -3,7 +3,26 @@
 def post_evaluation(expression):
     stack = []
 
-
+# Split expression into operands/operators
+    tokens = expression.split()
+    
+    for ch in tokens:
+        if ch.isdigit():  # Operand
+            stack.append(int(ch))
+        else:  # Operator
+            b = stack.pop()
+            a = stack.pop()
+            
+            if ch == '+':
+                stack.append(a + b)
+            elif ch == '-':
+                stack.append(a - b)
+            elif ch == '*':
+                stack.append(a * b)
+            elif ch == '/':
+                stack.append(a / b)
+    
+    return stack.pop()
 
 
 # taking the input of the expression
